48d322ee186c4ca1b1c8ac10bc19b90d9a515c91
HDTali <popovychnatalia88@gmail.com>
2026-06-14 10:15:29 +0200

    fix26: white inactive gates in defined centers, gate 45 closer to edge [53,27]

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const CENTER_MAP = {
   spleen:'Spleen', root:'Root'
 };
 
-const VERSION = 'v20260614-fix25';
+const VERSION = 'v20260614-fix26';
 
 /**
  * Конвертирует personality/design в map {planet: obj}.
